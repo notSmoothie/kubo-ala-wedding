c84fad7bb6787d473452ecbf8435c2eef8865c31
Martin Thomas Ondruska <martin.ondruska@kajot.cz>
2024-09-16 18:50:39 +0200

    add: page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="card">
+      <img
+        src="./assets/flipborder.png"
+        style="
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          top: 38%;
+          left: 63%;
+          width: 65vw;
+        "
+      />
+      <!-- <img
+        src="./assets/border1.png"
+        style="
+          position: absolute;
+          top: 50%;
+          z-index: 3;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          top: 38%;
+          left: 63%;
+          width: 65vw;
+        "
+      />
+      <img
+        src="./assets/border2.png"
+        style="
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          top: 38%;
+          left: 63%;
+          width: 65vw;
+        "
+      /> -->
+      <div class="card-left">
+        <img src="./assets/pic.jpg" alt="Couple Photo" class="couple-photo" />
+      </div>
+      <div class="card-right">
+        <div class="contentwrap">
+          <h1 class="heading">Alenka a Jakub</h1>
+          <p class="invitation-text">
+            Join us for the wedding<br />
+            <b>Date:</b> July 20th, 2024<br />
+            <b>Venue:</b> The Grand Venue<br />
+            <b>Time:</b> 5:00 PM<br /><br />
+            We can't wait to celebrate our big day with you! Please RSVP using the link below.<br /><br />
+          </p>
+          <a href="your-rsvp-link" class="rsvp-button">RSVP Here</a>
+        </div>
+      </div>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
